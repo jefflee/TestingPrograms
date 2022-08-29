05b2d795c55f8568a576e97d80750c667354a83d
Jeff Lee <jeff.lee@redcypress.com.tw>
2022-08-29 11:33:02 +0800

    StringEquals

---
+++ StringEquals/Program.cs
@@ -0,0 +1,52 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
+
+namespace StringEquals
+{
+    public class Program
+    {
+        private readonly string[] _tokens = { "This", "is", "a", "pen", "This", "is", "an", "apple." };
+
+        static void Main(string[] args)
+        {
+            // Do benchmark test
+            // BenchmarkTest();
+
+            // Word count test
+            new WordCounter().PerformanceTest();
+        }
+
+        private static void BenchmarkTest()
+        {
+            var summary = BenchmarkRunner.Run<Program>();
+        }
+
+        [Benchmark]
+        public int StringCountWithOrdinalIgnoreCase()
+        {
+            return StringCount("a", StringComparison.OrdinalIgnoreCase);
+        }
+
+        [Benchmark]
+
+        public int StringCountWithInvariantCultureIgnoreCase()
+        {
+            return StringCount("a", StringComparison.InvariantCultureIgnoreCase);
+        }
+
+        private int StringCount(string comparedString, StringComparison comparision)
+        {
+            int count = 0;
+
+            foreach (var token in _tokens)
+            {
+                if (token.Equals(comparedString, comparision))
+                {
+                    count++;
+                }
+            }
+
+            return count;
+        }
+    }
+}
---
+++ StringEquals/WordCounter.cs
@@ -0,0 +1,54 @@
+﻿using System.Diagnostics;
+
+namespace StringEquals
+{
+    internal class WordCounter
+    {
+        private readonly string _textFile = ".\\Data\\rfc6749.txt";
+        private readonly string[] _tokens;
+
+        public WordCounter()
+        {
+            var wholeText = File.ReadAllText(_textFile);
+            _tokens = wholeText.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+        }
+
+        public void PerformanceTest()
+        {
+            StopWatchWrapper(CountWordsWithOrdinalIgnoreCase);
+            StopWatchWrapper(CountWordsWithInvariantCultureIgnoreCase);
+
+        }
+
+        private int CountWordsWithOrdinalIgnoreCase()
+        {
+            return CountWords(StringComparison.OrdinalIgnoreCase);
+        }
+
+        private int CountWordsWithInvariantCultureIgnoreCase()
+        {
+            return CountWords(StringComparison.InvariantCultureIgnoreCase);
+        }
+
+        private int StopWatchWrapper(Func<int> countWordFunc)
+        {
+            Stopwatch stopWatch = new Stopwatch();
+            stopWatch.Start();
+            int count = countWordFunc();
+            stopWatch.Stop();
+
+            TimeSpan ts = stopWatch.Elapsed;
+            string elapsedTime = String.Format("{0:00}:{1:00}:{2:00}.{3:00}",
+                ts.Hours, ts.Minutes, ts.Seconds,
+                ts.Milliseconds / 10);
+            Console.WriteLine($"{countWordFunc.Method.Name}() RunTime " + elapsedTime);
+
+            return count;
+        }
+
+        private int CountWords(StringComparison comparision)
+        {
+            return _tokens.Count(token => token.Equals("oauth", comparision));
+        }
+    }
+}
