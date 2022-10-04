2c32af1e6a9496abcf3759eaf16c9a4ce081264a
JEFFZ16\Jeff <hellow.jeff@gmail.com>
2022-10-04 15:27:47 +0800

    ImageSharp | Handle background color while converting PNG to JPEG

---
+++ TestingPrograms/ImageSharp/ImageSharpTests.cs
@@ -0,0 +1,26 @@
+﻿using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.Processing;
+
+namespace TestingPrograms.ImageSharp
+{
+    [TestFixture]
+    internal class ImageSharpTests
+    {
+        [Test]
+        public void ConvertPngToJpeg()
+        {
+            using (Image image = Image.Load("ImageSharp/Data/testPic.png"))
+            {
+                int width = image.Width;
+                int height = image.Height;
+                image.Mutate(x =>
+                {
+                    x.Resize(width, height);
+                    x.BackgroundColor(Color.White);
+                });
+
+                image.Save("ImageSharp/Data/output.jpg");
+            }
+        }
+    }
+}
