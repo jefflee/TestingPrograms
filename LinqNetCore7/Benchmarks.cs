629064ff5a15b555eae69825863e9c638a628eb0
Jeff Lee <jeff.lee@redcypress.com.tw>
2022-10-21 08:33:51 +0800

    Linq performance testing.

---
+++ LinqNetCore7/Benchmarks.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Jobs;
+
+namespace LinqNetCore7
+{
+    [SimpleJob(RuntimeMoniker.Net60)]
+    [MemoryDiagnoser(false)]
+    public class Benchmarks
+    {
+        [Params(1000)]
+        public int Size { get; set; }
+
+        private IEnumerable<int> _items;
+
+        [GlobalSetup]
+        public void Setup()
+        {
+            _items = Enumerable.Range(1, Size).ToArray();
+        }
+
+        [Benchmark]
+        public int Min() => _items.Min();
+
+        [Benchmark]
+        public int Max() => _items.Max();
+
+        [Benchmark]
+        public double Average() => _items.Average();
+
+        [Benchmark]
+        public int Sum() => _items.Sum();
+    }
+}
