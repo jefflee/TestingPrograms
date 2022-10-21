3ee5dc580657778817eddac83bae0723a4694242
Jeff Lee <jeff.lee@redcypress.com.tw>
2022-10-21 09:02:44 +0800

    Add AutoFixture

---
+++ AutoFixture.Tests/FirstTests.cs
@@ -0,0 +1,54 @@
+using AutoFixture.Tests.TestingClasses;
+
+namespace AutoFixture.Tests
+{
+    public class FirstTests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void IntCalculator_Success_WithoutAutoFixture()
+        {
+            // arrange
+            var sut = new IntCalculator();
+
+            // act
+            sut.Subtract(1);
+
+            // assert
+            Assert.True(sut.Value < 0);
+        }
+
+        [Test]
+        public void IntCalculator_Success_WithAutoFixture()
+        {
+            // arrange
+            var sut = new IntCalculator();
+            var fixture = new Fixture();
+
+            // act
+            sut.Subtract(fixture.Create<int>());
+
+            // assert
+            Assert.True(sut.Value < 0);
+        }
+
+        [Test]
+        public void IntCalculator_Success_GiveAnIntRangeWithAutoFixture()
+        {
+            // arrange
+            var sut = new IntCalculator();
+            var fixture = new Fixture();
+            fixture.Customizations.Add(new RandomNumericSequenceGenerator(1, 1000));
+
+            // act
+            sut.Subtract(fixture.Create<int>());
+
+            // assert
+            Assert.True(sut.Value < 0);
+        }
+    }
+}
