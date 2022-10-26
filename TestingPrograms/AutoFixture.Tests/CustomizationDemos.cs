878de46f293e9e871fab7050bf13a3a23ab38ee9
Jeff Lee <jeff.lee@redcypress.com.tw>
2022-10-26 09:01:29 +0800

    AutoFixture | DateTimeCustomization

---
+++ TestingPrograms/AutoFixture.Tests/CustomizationDemos.cs
@@ -0,0 +1,22 @@
+﻿using AutoFixture;
+
+namespace TestingPrograms.AutoFixture.Tests
+{
+    public class CustomizationDemos
+    {
+        [Test]
+        public void DateTimeCustomization_Success_GetCurrentDateTime()
+        {
+            // arrange
+            var fixture = new Fixture();
+
+            //fixture.Customize(new CurrentDateTimeCustomization());
+            fixture.Customizations.Add(new CurrentDateTimeGenerator());
+
+            var date1 = fixture.Create<DateTime>();
+            var date2 = fixture.Create<DateTime>();
+
+            date1.Date.Should().Be(date2.Date);
+        }
+    }
+}
