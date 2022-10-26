9e1802a71ee2d1e8fa3668bb0d3a51546f9050b7
Jeff Lee <jeff.lee@redcypress.com.tw>
2022-10-26 10:04:46 +0800

    AutoFixture | Custom specimen Builder

---
+++ TestingPrograms/AutoFixture.Tests/AutoFixtureGenerators/AirportCodeStringPropertyGenerator.cs
@@ -0,0 +1,42 @@
+﻿using System.Reflection;
+using AutoFixture.Kernel;
+
+namespace TestingPrograms.AutoFixture.Tests.AutoFixtureGenerators
+{
+    public class AirportCodeStringPropertyGenerator : ISpecimenBuilder
+    {
+        public object Create(object request, ISpecimenContext context)
+        {
+            // See if we are trying to create a value for a property
+            var propertyInfo = request as PropertyInfo;
+
+            if (propertyInfo is null)
+            {
+                // this specimen builder does not apply to current request
+                return new NoSpecimen(); // Null is a valid specimen so return NoSpecimen
+            }
+
+            // Now we know we're dealing with a property, are we creating 
+            // a value for an airport code?
+            var isAirportCodeProperty = propertyInfo.Name.Contains("AirportCode");
+            var isStringProperty = propertyInfo.PropertyType == typeof(string);
+
+            if (isAirportCodeProperty && isStringProperty)
+            {
+                return RandomAirportCode();
+            }
+
+            return new NoSpecimen();
+        }
+
+        private string RandomAirportCode()
+        {
+            if (DateTime.Now.Ticks % 2 == 0)
+            {
+                return "LHR";
+            }
+
+            return "PER";
+        }
+    }
+}
--- TestingPrograms/AutoFixture.Tests/CustomizationDemos.cs
+++ TestingPrograms/AutoFixture.Tests/CustomizationDemos.cs
@@ -1,11 +1,14 @@
 ﻿using AutoFixture;
+using FluentAssertions.Execution;
+using TestingPrograms.AutoFixture.Tests.AutoFixtureGenerators;
+using TestingPrograms.AutoFixture.Tests.TestingClasses;
 
 namespace TestingPrograms.AutoFixture.Tests
 {
     public class CustomizationDemos
     {
         [Test]
-        public void DateTimeCustomization_Success_GetCurrentDateTime()
+        public void CreateDateTime_Success_GetCurrentDateTime()
         {
             // arrange
             var fixture = new Fixture();
@@ -17,6 +20,26 @@ namespace TestingPrograms.AutoFixture.Tests
             var date2 = fixture.Create<DateTime>();
 
             date1.Date.Should().Be(date2.Date);
+        }
+
+        [Test]
+        public void Create_Success_WithCustomSpecimenBuilder()
+        {
+            // arrange
+            var fixture = new Fixture();
+            fixture.Customizations.Add(new AirportCodeStringPropertyGenerator());
+
+            var flight = fixture.Create<FlightDetails>();
+            var airport = fixture.Create<Airport>();
+
+            // assert
+            string[] airportCodes = { "LHR", "PER" };
+            using (new AssertionScope())
+            {
+                flight.DepartureAirportCode.Should().BeOneOf(airportCodes);
+                flight.ArrivalAirportCode.Should().BeOneOf(airportCodes);
+                airport.AirportCode.Should().BeOneOf(airportCodes);
+            }
         }
     }
 }
