3afd035d73ca291aa806387af8b563e6c1d5964a
JEFFZ16\Jeff <hellow.jeff@gmail.com>
2022-11-27 09:29:14 +0800

    Calling async method in sync method

---
+++ TestingPrograms/AsyncException/DbRepository.cs
@@ -0,0 +1,64 @@
+﻿using Nito.AsyncEx;
+using Nito.AsyncEx.Synchronous;
+
+namespace TestingPrograms.AsyncException
+{
+    internal class DbRepository
+    {
+        public async Task<String> GetStringAsync()
+        {
+            throw new NotImplementedException("This is not implemented");
+        }
+
+        public void CallingWithResult()
+        {
+            var r = GetStringAsync().Result;
+        }
+
+        public async Task CallingWithAwait()
+        {
+            await GetStringAsync();
+        }
+
+        /// <summary>
+        /// If you have a simple asynchronous method that doesn't need to synchronize back to its context,
+        /// then you can use Task.WaitAndUnwrapException
+        /// Refer to https://stackoverflow.com/a/9343733 Solution A
+        /// </summary>
+        public void CallingWithWaitAndUnwrapException()
+        {
+            GetStringAsync().WaitAndUnwrapException();
+        }
+
+        /// <summary>
+        /// If "GetStringAsync" does need to synchronize back to its context,
+        /// then you may be able to use AsyncContext.RunTask to provide a nested context
+        /// Refer to https://stackoverflow.com/a/9343733 Solution B
+        /// </summary>
+        public void CallingWithAsyncContext()
+        {
+            var r = AsyncContext.Run(GetStringAsync);
+        }
+
+        /// <summary>
+        /// AsyncContext.RunTask won't work in every scenario.
+        /// For example, if the async method awaits something that requires a UI event to complete,
+        /// then you'll deadlock even with the nested context.
+        /// In that case, you could start the async method on the thread pool:
+        /// Refer to https://stackoverflow.com/a/9343733 Solution C
+        /// </summary>
+        public void CallingWithTaskAndNitoAsyncEx()
+        {
+            var task = Task.Run(async () => await GetStringAsync());
+            var r = task.WaitAndUnwrapException();
+        }
+
+        public void CallingWithTask()
+        {
+            var task = Task.Run(
+                async () => await GetStringAsync());
+            var r = task.Result;
+
+        }
+    }
+}
---
+++ TestingPrograms/AsyncException/DbRepositoryTest.cs
@@ -0,0 +1,121 @@
+﻿using FluentAssertions.Execution;
+
+namespace TestingPrograms.AsyncException
+{
+    [TestFixture]
+    internal class DbRepositoryTest
+    {
+
+        [Test]
+        public void CallingWithResult_Exception_BothCheckCanBePassed()
+        {
+            var repository = new DbRepository();
+            Action act = () => repository.CallingWithResult();
+            using (new AssertionScope())
+            {
+                act.Should().Throw<AggregateException>()
+                    .WithMessage("One or more errors occurred. (This is not implemented)");
+                act.Should().Throw<NotImplementedException>().WithMessage("This is not implemented");
+            }
+        }
+
+        [Test]
+        public void CallingWithResult_Exception_TypeIsAggregateException()
+        {
+            var repository = new DbRepository();
+            try
+            {
+                repository.CallingWithResult();
+            }
+            catch (Exception ex)
+            {
+                // Assert
+                // Exception type is AggregateException
+                ex.GetType().Should().Be<AggregateException>();
+                ex.GetType().Should().NotBe<NotImplementedException>();
+                ex.Message.Should().Be("One or more errors occurred. (This is not implemented)");
+
+                // Inner Exception type is NotImplementedException
+                Exception innerEx = ex!.InnerException;
+                innerEx!.GetType().Should().Be<NotImplementedException>();
+                innerEx.Message.Should().Be("This is not implemented");
+            }
+        }
+
+        [Test]
+        public void CallingWithAwait_Exception_TypeIsNotImplementedException()
+        {
+            var repository = new DbRepository();
+            Func<Task> func = async () => await repository.CallingWithAwait();
+            func.Should().ThrowAsync<NotImplementedException>().WithMessage("* NOT IMPLEMENTED");
+        }
+
+        [Test]
+        public void CallingWithWaitAndUnwrapException_Exception_TypeIsNotImplementedException()
+        {
+            var repository = new DbRepository();
+            try
+            {
+                repository.CallingWithWaitAndUnwrapException();
+            }
+            catch (Exception ex)
+            {
+                // Assert
+                ex.GetType().Should().Be<NotImplementedException>();
+                ex.GetType().Should().NotBe<AggregateException>();
+                ex.Message.Should().Be("This is not implemented");
+            }
+        }
+
+        [Test]
+        public void CallingWithAsyncContext_Exception_TypeIsNotImplementedException()
+        {
+            var repository = new DbRepository();
+            try
+            {
+                repository.CallingWithAsyncContext();
+            }
+            catch (Exception ex)
+            {
+                // Assert
+                ex.GetType().Should().Be<NotImplementedException>();
+                ex.GetType().Should().NotBe<AggregateException>();
+                ex.Message.Should().Be("This is not implemented");
+            }
+        }
+
+        [Test]
+        public void CallingWithTaskAndNitoAsyncEx_Exception_TypeIsNotImplementedException()
+        {
+            var repository = new DbRepository();
+            try
+            {
+                repository.CallingWithTaskAndNitoAsyncEx();
+            }
+            catch (Exception ex)
+            {
+                // Assert
+                ex.GetType().Should().Be<NotImplementedException>();
+                ex.GetType().Should().NotBe<AggregateException>();
+                ex.Message.Should().Be("This is not implemented");
+            }
+        }
+
+        [Test]
+        public void CallingWithTask_Exception_TypeIsNotImplementedException()
+        {
+            var repository = new DbRepository();
+            try
+            {
+                repository.CallingWithTask();
+            }
+            catch (Exception ex)
+            {
+                // Assert
+                ex.GetType().Should().Be<AggregateException>();
+                ex.GetType().Should().NotBe<NotImplementedException>();
+                ex.Message.Should().Be("One or more errors occurred. (This is not implemented)");
+            }
+        }
+    }
+}
