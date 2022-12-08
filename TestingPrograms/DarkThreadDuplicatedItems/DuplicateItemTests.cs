e9d3b0123944acbcb1744e7522e4c31567cdeaf2
JEFFZ16\Jeff <hellow.jeff@gmail.com>
2022-12-08 10:32:53 +0800

    C# practice - Find duplicated items

---
+++ TestingPrograms/DarkThreadDuplicatedItems/DuplicateItemTests.cs
@@ -0,0 +1,61 @@
+﻿namespace TestingPrograms.DarkThreadDuplicatedItems
+{
+    [TestFixture]
+    internal class DuplicateItemTests
+    {
+        [Test]
+        public void FindDuplicatedItems_GetDuplicatedString_GiveAnStringArray()
+        {
+            string[] input = new String[] { "A", "B", "B", "C", "X", "C", "C", "B", "B", "D", "D", "D" };
+            string[][] expected =
+            {
+                new[] { "B", "B" },
+                new[] { "C", "C" },
+                new[] { "B", "B" },
+                new[] { "D", "D","D" },
+            };
+
+            var sut = new DuplicateItems();
+            var result = sut.FindDuplicatedItems(input, EqualityComparer<string>.Default);
+            result.Should().BeEquivalentTo(expected, option => option.WithStrictOrdering());
+        }
+
+        [Test]
+        public void FindDuplicatedItems_GetDuplicatedInt_GiveAnIntArray()
+        {
+            int[] input = new int[] { 1, 1, 1, 5, 5, 6, 7, 2, 3, 5, 5, 5 };
+            int[][] expected =
+            {
+                new[] { 1,1,1 },
+                new[] { 5,5 },
+                new[] { 5,5,5},
+            };
+
+            var sut = new DuplicateItems();
+            var result = sut.FindDuplicatedItems(input, EqualityComparer<int>.Default);
+            result.Should().BeEquivalentTo(expected);
+        }
+
+        [Test]
+        public void FindDuplicatedItems_NoError_GiveAnOneElementArray()
+        {
+            string[] input = new String[] { "A" };
+            var expected = Array.Empty<string>();
+
+            var sut = new DuplicateItems();
+            var result = sut.FindDuplicatedItems(input, EqualityComparer<string>.Default);
+            result.Should().BeEquivalentTo(expected, option => option.WithStrictOrdering());
+        }
+
+        [Test]
+        public void FindDuplicatedItems_NoError_GiveAnEmptyArray()
+        {
+            string[] input = Array.Empty<string>();
+            var expected = Array.Empty<string>();
+
+            var sut = new DuplicateItems();
+            var result = sut.FindDuplicatedItems(input, EqualityComparer<string>.Default);
+            result.Should().BeEquivalentTo(expected, option => option.WithStrictOrdering());
+        }
+    }
+}
---
+++ TestingPrograms/DarkThreadDuplicatedItems/DuplicateItems.cs
@@ -0,0 +1,43 @@
+﻿namespace TestingPrograms.DarkThreadDuplicatedItems
+{
+    /// <summary>
+    /// 題問：想在陣列挑出連續重複出現的項目，例如要從陣列 [A, B, B, C, X, C, C, B, B, D, D, D] 挑出 [B, B]、[C, C]、[B, B]、[D, D, D] 四個群組。
+    /// https://blog.darkthread.net/blog/find-repeated-item-group/?fbclid=IwAR17AGBrS-1WfCA4Ov1sf4QDDJqXq6RjvxNwoaTwjwU2RsOcbmv53tGyp4A
+    ///
+    /// Joey
+    /// https://gist.github.com/hatelove/355b0740f9f76d1187dc61d6bb21b4fa?fbclid=IwAR1R9mfYQ64Fs4UwB_BiY-Wo_RQBemFBaH6hTIFmx-A4TqX1KEd5CffkjYE
+    /// </summary>
+    internal class DuplicateItems
+    {
+
+        public IEnumerable<IEnumerable<T>> FindDuplicatedItems<T>(IEnumerable<T> collection, IEqualityComparer<T> equalityComparer)
+        {
+            using var enumerator = collection.GetEnumerator();
+            if (!enumerator.MoveNext())
+            {
+                yield break;
+            }
+
+            var dupList = new List<T> { enumerator.Current };
+            while (enumerator.MoveNext())
+            {
+                var current = enumerator.Current;
+                if (!equalityComparer.Equals(current, dupList.First()))
+                {
+                    if (dupList.Count > 1)
+                    {
+                        yield return dupList;
+                    }
+                    dupList = new List<T>();
+                }
+
+                dupList.Add(current);
+            }
+
+            if (dupList.Count > 1)
+            {
+                yield return dupList;
+            }
+        }
+    }
+}
