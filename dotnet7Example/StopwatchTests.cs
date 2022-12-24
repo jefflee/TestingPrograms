c9e2ae5f98cbbf297b73c2d73f96e84f52921c1c
DFTECH\jeffl <jeff.lee@redcypress.com.tw>
2022-12-24 11:58:45 +0800

    This is a new way of using Stopwatch in .Net 7. It saves memory

---
+++ dotnet7Example/StopwatchTests.cs
@@ -0,0 +1,25 @@
+﻿using System.Diagnostics;
+
+namespace Dotnet7Example
+{
+    [TestFixture]
+    public class StopwatchTests
+    {
+        /// <summary>
+        /// This is a new way of using Stopwatch in .Net 7. It saves memory
+        /// Refer to https://www.youtube.com/watch?v=NTz99yN2urc
+        /// </summary>
+        /// <returns></returns>
+        [Test()]
+        public async Task GetElapsedTime_GetTimeSpan_WithNewDotNet7Methods()
+        {
+            var starTime = Stopwatch.GetTimestamp();
+
+            //do something here
+            await Task.Delay(1000);
+
+            TimeSpan diff = Stopwatch.GetElapsedTime(starTime);
+            Console.WriteLine(diff);
+        }
+    }
+}
