7fa7707e162d6514e4b312220f7be92d2c3b6616
DFTECH\jeffl <jeff.lee@redcypress.com.tw>
2022-12-31 10:39:52 +0800

    Another case that access a shared HashSet

--- TestingPrograms/LinqWithAsyncFunction.cs
+++ TestingPrograms/LinqWithAsyncFunction.cs
@@ -3,9 +3,12 @@
     [TestFixture]
     public class LinqWithAsyncFunction
     {
+        private static readonly HashSet<int> _hashSet = new HashSet<int>();
+
         [Test]
         public async Task Select_ItIsParallel_WithTaskWhenAll()
         {
+            _hashSet.Clear();
             var intArray = Enumerable.Range(1, 100);
             await Task.WhenAll(
                 intArray.Select(async k =>
@@ -14,6 +17,7 @@
                     await PrintNumberAsync(k);
                 })).ConfigureAwait(false);
         }
+
 
         /// <summary>
         /// Print nothing, because there is a Task.Delay in PrintNumber().
@@ -32,10 +36,37 @@
             });
         }
 
+
         private static async Task PrintNumberAsync(int k)
         {
             await Task.Delay(10);
             Console.WriteLine("PrintNumber(). k=" + k);
+        }
+
+        /// <summary>
+        /// Access a shared HashSet. So, sometime it gets an error.
+        /// Message:
+        /// System.ArgumentException : Destination array was not long enough.Check the destination index, length, and the array's lower bounds. (Parameter 'destinationArray')
+        /// </summary>
+        /// <returns></returns>
+        // [Test]
+        public async Task Select_SometimesError_WhenAccessASharedHashSet()
+        {
+            _hashSet.Clear();
+            var intArray = Enumerable.Range(1, 100);
+            await Task.WhenAll(
+                intArray.Select(async k =>
+                {
+                    Console.WriteLine("In the Select function. k=" + k);
+                    await AddToHashSetAsync(k);
+                })).ConfigureAwait(false);
+        }
+
+        private static async Task AddToHashSetAsync(int k)
+        {
+            await Task.Delay(10);
+            _hashSet.Add(k);
+            Console.WriteLine("AddToHashSetAsync(). k=" + k);
         }
     }
 }
