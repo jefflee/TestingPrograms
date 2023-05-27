24904e6dfc876e27769c4bcab40aed151febfc6d
JEFFZ16\Jeff <hellow.jeff@gmail.com>
2023-05-27 11:04:24 +0800

    refactor

--- TestingPrograms/AutoFixture.Tests/TestingClasses/PlayerCharacter.cs
+++ TestingPrograms/AutoFixture.Tests/TestingClasses/PlayerCharacter.cs
@@ -1,17 +1,14 @@
 ﻿using System.ComponentModel.DataAnnotations;
-using RangeAttribute = System.ComponentModel.DataAnnotations.RangeAttribute;
 
 namespace TestingPrograms.AutoFixture.Tests.TestingClasses
 {
     public class PlayerCharacter
     {
-        [StringLength(20)]
-        public string RealName { get; set; }
+        [StringLength(20)] public string RealName { get; set; } = string.Empty;
 
-        [StringLength(8)]
-        public string GameCharacterName { get; set; }
+        [StringLength(8)] public string GameCharacterName { get; set; } = string.Empty;
 
-        [Range(0, 10)]
+        [System.ComponentModel.DataAnnotations.Range(0, 10)]
         public int CurrentHealth { get; set; }
     }
 }
