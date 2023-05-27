edbc2d087790b0ce2965106493d0838c1b690478
JEFFZ16\Jeff <hellow.jeff@gmail.com>
2023-05-27 11:07:00 +0800

    refactor

--- TestingPrograms/AutoFixture.Tests/TestingClasses/FlightDetails.cs
+++ TestingPrograms/AutoFixture.Tests/TestingClasses/FlightDetails.cs
@@ -5,10 +5,10 @@ namespace TestingPrograms.AutoFixture.Tests.TestingClasses
     [ExcludeFromCodeCoverage]
     public class FlightDetails
     {
-        private string _arrivalAirportCode;
-        private string _departureAirportCode;
+        private string? _arrivalAirportCode;
+        private string? _departureAirportCode;
 
-        public string DepartureAirportCode
+        public string? DepartureAirportCode
         {
             get => _departureAirportCode;
             set
@@ -18,7 +18,7 @@ namespace TestingPrograms.AutoFixture.Tests.TestingClasses
             }
         }
 
-        public string ArrivalAirportCode
+        public string? ArrivalAirportCode
         {
             get => _arrivalAirportCode;
             set
@@ -29,14 +29,14 @@ namespace TestingPrograms.AutoFixture.Tests.TestingClasses
         }
 
         public TimeSpan FlightDuration { get; set; }
-        public string AirlineName { get; set; }
-        public List<string> MealOptions { get; set; } = new List<string>();
+        public string? AirlineName { get; set; }
+        public List<string> MealOptions { get; set; } = new();
 
-        private void EnsureValidAirportCode(string airportCode)
+        private void EnsureValidAirportCode(string? airportCode)
         {
-            var isWrongLength = airportCode.Length != 3;
+            var isWrongLength = airportCode?.Length != 3;
 
-            var isWrongCase = airportCode != airportCode.ToUpperInvariant();
+            var isWrongCase = airportCode != airportCode?.ToUpperInvariant();
 
             if (isWrongLength || isWrongCase)
             {
