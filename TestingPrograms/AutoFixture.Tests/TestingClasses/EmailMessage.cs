583956379721eaee72daf9c5db01d17153da7af8
JEFFZ16\Jeff <hellow.jeff@gmail.com>
2023-05-27 11:03:10 +0800

    refactor

--- TestingPrograms/AutoFixture.Tests/TestingClasses/EmailMessage.cs
+++ TestingPrograms/AutoFixture.Tests/TestingClasses/EmailMessage.cs
@@ -2,25 +2,29 @@
 {
     public class EmailMessage
     {
-        private string _somePrivateField;
+        private readonly string _somePrivateField;
 
         public string SomePublicField;
-        private string SomePrivateProperty { get; set; }
-        public Guid Id { get; set; }
-        public string ToAddress { get; set; }
-        public string MessageBody { get; private set; }
-        public string Subject { get; set; }
-        public bool IsImportant { get; set; }
-
-        public EmailMessageType MessageType { get; set; }
 
         public EmailMessage(string toAddress, string messageBody, bool isImportant)
         {
             ToAddress = toAddress;
             MessageBody = messageBody;
             IsImportant = isImportant;
+            SomePublicField = string.Empty;
+            SomePrivateProperty = string.Empty;
+            Subject = string.Empty;
             _somePrivateField = "test";
         }
+
+        private string SomePrivateProperty { get; }
+        public Guid Id { get; set; }
+        public string ToAddress { get; set; }
+        public string MessageBody { get; }
+        public string Subject { get; set; }
+        public bool IsImportant { get; set; }
+
+        public EmailMessageType MessageType { get; set; }
 
         public string GetSomePrivateProperty()
         {
@@ -32,6 +36,7 @@
             return _somePrivateField;
         }
     }
+
     public enum EmailMessageType
     {
         Unspecified,
