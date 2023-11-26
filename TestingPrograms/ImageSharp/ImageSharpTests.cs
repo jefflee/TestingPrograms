84abd354e97a59ece672eae58e7972d054a6fc42
JEFFZ16\Jeff <hellow.jeff@gmail.com>
2023-11-26 09:24:36 +0800

    update ImageSharp cases. png without background

--- TestingPrograms/ImageSharp/ImageSharpTests.cs
+++ TestingPrograms/ImageSharp/ImageSharpTests.cs
@@ -1,26 +1,39 @@
-﻿using SixLabors.ImageSharp;
-using SixLabors.ImageSharp.Processing;
-
-namespace TestingPrograms.ImageSharp
+﻿namespace TestingPrograms.ImageSharp
 {
     [TestFixture]
     internal class ImageSharpTests
     {
         [Test]
-        public void ConvertPngToJpeg()
+        public void Mutate_JpgFile_WhenGiveANoBackgroundPngFile()
         {
-            using (Image image = Image.Load("ImageSharp/Data/testPic.png"))
-            {
-                int width = image.Width;
-                int height = image.Height;
-                image.Mutate(x =>
-                {
-                    x.Resize(width, height);
-                    x.BackgroundColor(Color.White);
-                });
+            using var image = Image.Load("ImageSharp/Data/testPic.png");
+            var width = image.Width;
+            var height = image.Height;
 
-                image.Save("ImageSharp/Data/output.jpg");
-            }
+            image.Mutate(x =>
+            {
+                x.Resize(width, height);
+                x.BackgroundColor(Color.White);
+            });
+
+            image.Save("ImageSharp/Data/UT1_output_white_bg.jpg");
+        }
+
+        [Test]
+        public void Mutate_PngFile_WhenGiveANoBackgroundPngFile()
+        {
+            using var image = Image.Load("ImageSharp/Data/testPic.png");
+            var width = image.Width;
+            var height = image.Height;
+            var resizeOptions = new ResizeOptions
+            {
+                Mode = ResizeMode.Max,
+                PremultiplyAlpha = false,
+                Size = new Size(width, height)
+            };
+            image.Mutate(x => { x.Resize(resizeOptions); });
+
+            image.Save("ImageSharp/Data/UT2_output_no_bg.png");
         }
     }
 }
