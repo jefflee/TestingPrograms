36c5346d2df20440191bd6d33303430d60a6178f
DFTECH\jeffl <jeff.lee@redcypress.com.tw>
2024-03-12 11:56:14 +0800

    Upgrade ImageSharp package

--- TestingPrograms/ImageSharp/ImageSharpTests.cs
+++ TestingPrograms/ImageSharp/ImageSharpTests.cs
@@ -1,39 +1,41 @@
-﻿namespace TestingPrograms.ImageSharp
+﻿using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.Processing;
+
+namespace TestingPrograms.ImageSharp;
+
+[TestFixture]
+internal class ImageSharpTests
 {
-    [TestFixture]
-    internal class ImageSharpTests
+    [Test]
+    public void Mutate_JpgFile_WhenGiveANoBackgroundPngFile()
     {
-        [Test]
-        public void Mutate_JpgFile_WhenGiveANoBackgroundPngFile()
+        using var image = Image.Load("ImageSharp/Data/testPic.png");
+        var width = image.Width;
+        var height = image.Height;
+
+        image.Mutate(x =>
         {
-            using var image = Image.Load("ImageSharp/Data/testPic.png");
-            var width = image.Width;
-            var height = image.Height;
+            x.Resize(width, height);
+            x.BackgroundColor(Color.White);
+        });
 
-            image.Mutate(x =>
-            {
-                x.Resize(width, height);
-                x.BackgroundColor(Color.White);
-            });
+        image.Save("ImageSharp/Data/UT1_output_white_bg.jpg");
+    }
 
-            image.Save("ImageSharp/Data/UT1_output_white_bg.jpg");
-        }
-
-        [Test]
-        public void Mutate_PngFile_WhenGiveANoBackgroundPngFile()
+    [Test]
+    public void Mutate_PngFile_WhenGiveANoBackgroundPngFile()
+    {
+        using var image = Image.Load("ImageSharp/Data/testPic.png");
+        var width = image.Width;
+        var height = image.Height;
+        var resizeOptions = new ResizeOptions
         {
-            using var image = Image.Load("ImageSharp/Data/testPic.png");
-            var width = image.Width;
-            var height = image.Height;
-            var resizeOptions = new ResizeOptions
-            {
-                Mode = ResizeMode.Max,
-                PremultiplyAlpha = false,
-                Size = new Size(width, height)
-            };
-            image.Mutate(x => { x.Resize(resizeOptions); });
+            Mode = ResizeMode.Max,
+            PremultiplyAlpha = false,
+            Size = new Size(width, height)
+        };
+        image.Mutate(x => { x.Resize(resizeOptions); });
 
-            image.Save("ImageSharp/Data/UT2_output_no_bg.png");
-        }
+        image.Save("ImageSharp/Data/UT2_output_no_bg.png");
     }
 }
