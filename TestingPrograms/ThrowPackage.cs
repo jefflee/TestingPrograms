00f0c7e1138e7b4368560637d5bea28796e04646
JEFFZ16\Jeff <hellow.jeff@gmail.com>
2024-05-06 17:25:29 +0800

    Add Throw package

---
+++ TestingPrograms/ThrowPackage.cs
@@ -0,0 +1,49 @@
+﻿using Throw;
+
+namespace TestingPrograms;
+
+[TestFixture]
+public class ThrowPackage
+{
+    [Test]
+    public void Throw_DefaultException_WhenTheStringIsFoo()
+    {
+        void LocalFunc()
+        {
+            "foo".Throw().IfEquals("foo");
+        }
+
+        var act = () => LocalFunc();
+
+        act.Should().Throw<ArgumentException>();
+    }
+
+    [Test]
+    public void Throw_SpecificException_WhenTheStringIsFoo()
+    {
+        void LocalFunc()
+        {
+            "foo".Throw(() => new TimeoutException("test")).IfEquals("foo");
+        }
+
+        var act = () => LocalFunc();
+
+        act.Should().Throw<TimeoutException>();
+    }
+
+    [Test]
+    public void Throw_Exception_WhenTheListContains5()
+    {
+        void LocalFunc()
+        {
+            // Verify the contains integer 5.
+            var list = Enumerable.Range(1, 10);
+            var isExisted = list.Any(x => x == 5);
+            isExisted.Throw().IfTrue();
+        }
+
+        var act = LocalFunc;
+
+        act.Should().Throw<ArgumentException>();
+    }
+}
