72319eae1cf1f8601e43e16af3f76bd2b3b6433f
JEFFZ16\Jeff <hellow.jeff@gmail.com>
2025-04-04 17:47:53 +0800

    Add BCrypt hash example

---
+++ TestingPrograms/BCryptHelper/BCryptPasswordHelper.cs
@@ -0,0 +1,33 @@
+﻿using BCryptNet = BCrypt.Net.BCrypt;
+
+namespace TestingPrograms.BCryptHelper
+{
+    internal class BCryptPasswordHelper
+    {
+        // The log2 of the number of rounds of hashing to apply
+        // The bigger value cause the longer computation.
+        // default is 11.
+        private const int WorkFactor = 11;
+
+        /// <summary>
+        ///     Hash password
+        /// </summary>
+        /// <param name="plainPassword"></param>
+        /// <returns></returns>
+        public static string HashPassword(string plainPassword)
+        {
+            return BCryptNet.HashPassword(plainPassword, WorkFactor);
+        }
+
+        /// <summary>
+        ///     Verify password
+        /// </summary>
+        /// <param name="inputPassword"></param>
+        /// <param name="hashedPassword"></param>
+        /// <returns></returns>
+        public static bool VerifyPassword(string inputPassword, string hashedPassword)
+        {
+            return BCryptNet.Verify(inputPassword, hashedPassword);
+        }
+    }
+}
---
+++ TestingPrograms/BCryptHelper/BCryptPasswordHelperTest.cs
@@ -0,0 +1,39 @@
+﻿namespace TestingPrograms.BCryptHelper
+{
+    [TestFixture]
+    public class BCryptPasswordHelperTest
+    {
+        // hashed password by "Passoword1234"
+        private readonly string hashedPassword1234 = "$2a$11$e4gA7pWSXegSn13Qe6qa0uRVz3lkhzSH8pSB1G/gJVLpeyIDVsF7e";
+
+        [Test]
+        public void HashPassword_GetHashCode_WithAPassword()
+        {
+            var password = "Passoword1234";
+
+            var hashed1 = BCryptPasswordHelper.HashPassword(password);
+            var hashed2 = BCryptPasswordHelper.HashPassword(password);
+
+            hashed1.Should().NotBe(hashed2, "BCrypt hashed with salt");
+
+            Console.WriteLine($"hashed1：{hashed1}");
+            Console.WriteLine($"hashed2：{hashed2}");
+        }
+
+        [Test]
+        public void VerifyPassword_ReturnTrue_WithACorrectPassword()
+        {
+            var result = BCryptPasswordHelper.VerifyPassword("Passoword1234", hashedPassword1234);
+
+            result.Should().BeTrue();
+        }
+
+        [Test]
+        public void VerifyPassword_ReturnFalse_WithAWrongPassword()
+        {
+            var result = BCryptPasswordHelper.VerifyPassword("wrong password", hashedPassword1234);
+
+            result.Should().BeFalse();
+        }
+    }
+}
