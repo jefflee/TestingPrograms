7f515f16de3ab4d8338126b6a8b09296cc019c1d
JEFFZ16\Jeff <hellow.jeff@gmail.com>
2025-05-25 09:44:49 +0800

    Add span benchmarks

---
+++ SpanBenchmarks/Benchmarks.cs
@@ -0,0 +1,81 @@
+﻿using BenchmarkDotNet.Attributes;
+
+namespace SpanBenchmarks;
+
+[MemoryDiagnoser(false)]
+public class Benchmarks
+{
+    private static readonly Random Random = new();
+
+    [ParamsSource(nameof(RandomNumbers))] public double Number { get; set; }
+
+    public IEnumerable<double> RandomNumbers()
+    {
+        // Generate a new random double for each benchmark iteration
+        yield return Random.NextDouble() * 1_000_000; // Example: random double between 0 and 1,000,000
+    }
+
+    // Traditional way - Create many temqporary strings
+    [Benchmark]
+    public string FormatNumberTraditional()
+    {
+        var numStr = Number.ToString("F2");
+        if (numStr.Contains("."))
+        {
+            var parts = numStr.Split('.');
+            var intPart = parts[0];
+            var decPart = parts[1];
+
+            // Insert commas to the integer part
+            for (var i = intPart.Length - 3; i > 0; i -= 3)
+            {
+                intPart = intPart.Insert(i, ",");
+            }
+
+            return intPart + "." + decPart;
+        }
+
+        return numStr;
+    }
+
+    // Span way - reduce temporary string allocations
+    [Benchmark]
+    public string FormatNumberWithSpan()
+    {
+        Span<char> buffer = stackalloc char[32];
+
+        if (Number.TryFormat(buffer, out var written, "F2"))
+        {
+            var numSpan = buffer.Slice(0, written);
+
+            // Add the index of the decimal point
+            var dotIndex = numSpan.IndexOf('.');
+            if (dotIndex > 3)
+            {
+                // Insert commas to the integer part
+                Span<char> result = stackalloc char[48];
+                var resultIndex = 0;
+
+                for (var i = 0; i < dotIndex; i++)
+                {
+                    if (i > 0 && (dotIndex - i) % 3 == 0)
+                    {
+                        result[resultIndex++] = ',';
+                    }
+
+                    result[resultIndex++] = numSpan[i];
+                }
+
+                // copy the decimal part
+                numSpan.Slice(dotIndex).CopyTo(result.Slice(resultIndex));
+                resultIndex += numSpan.Length - dotIndex;
+
+                return result.Slice(0, resultIndex).ToString();
+            }
+
+            return numSpan.ToString();
+        }
+
+        return Number.ToString("F2");
+    }
+}
---
+++ SpanBenchmarks/Program.cs
@@ -0,0 +1,36 @@
+﻿using BenchmarkDotNet.Running;
+using SpanBenchmarks;
+
+BenchmarkRunner.Run<Benchmarks>();
+
+/*
+    // * Summary *
+
+   BenchmarkDotNet v0.15.0, Windows 11 (10.0.26100.4061/24H2/2024Update/HudsonValley)
+   12th Gen Intel Core i9-12900H 2.90GHz, 1 CPU, 20 logical and 14 physical cores
+   .NET SDK 9.0.203
+     [Host]     : .NET 9.0.4 (9.0.425.16305), X64 RyuJIT AVX2
+     DefaultJob : .NET 9.0.4 (9.0.425.16305), X64 RyuJIT AVX2
+
+
+   | Method                  | Number           | Mean     | Error   | StdDev  | Allocated |
+   |------------------------ |----------------- |---------:|--------:|--------:|----------:|
+   | FormatNumberTraditional | 562759.832431761 | 155.6 ns | 2.18 ns | 1.93 ns |     240 B |
+   | FormatNumberWithSpan    | 562759.832431761 | 123.3 ns | 1.90 ns | 1.78 ns |      48 B |
+
+   // * Hints *
+   Outliers
+     Benchmarks.FormatNumberTraditional: Default -> 1 outlier  was  removed (168.61 ns)
+     Benchmarks.FormatNumberWithSpan: Default    -> 1 outlier  was  detected (122.12 ns)
+
+   // * Legends *
+     Number    : Value of the 'Number' parameter
+     Mean      : Arithmetic mean of all measurements
+     Error     : Half of 99.9% confidence interval
+     StdDev    : Standard deviation of all measurements
+     Allocated : Allocated memory per single operation (managed only, inclusive, 1KB = 1024B)
+     1 ns      : 1 Nanosecond (0.000000001 sec)
+
+
+
+ */
